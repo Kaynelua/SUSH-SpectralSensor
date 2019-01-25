1165b0c4aa81a27b8ef0b4e510698373481784a0
patrickjohncyh <patrickjohncyh@icloud.com>
2019-01-25 11:09:04 +0000

    fix ledInd function

--- SpectralSensor.py
+++ SpectralSensor.py
@@ -2,11 +2,10 @@ import smbus
 from I2C import write,read
 
 class SpectralSensor:
-	
 	def __init__(self):
 		self.bus = smbus.SMBus(1)
 
-	def led_ind(state:bool):
+	def ledInd(self,state:bool):
 		reg = read(self.bus,0x07)
 		if(state):
 			write(self.bus,reg & 0x0F)
