0697e47ff5147cbe5a37c10337a3dc8249b8206b
kaynelua <luayinghao95@hotmail.com>
2019-01-25 11:56:07 +0000

    Read Violet light

--- SpectralSensor.py
+++ SpectralSensor.py
@@ -24,12 +24,10 @@ class SpectralSensor:
 		reg = read(self.bus,0x04)
 		return bool(reg & 0x02)
 
-	def readChan(self,chan:char):
-		while( not self.dataRead()):
+	def readChan(self,chan):
+		while( not self.dataReady()):
 			pass
 		if(chan=='V'):
 			hi = read(self.bus,0x08)
 			lo = read(self.bus,0x09)
-			return (hi << 16 | lo)
-
-			
+			return (hi << 8 | lo)
