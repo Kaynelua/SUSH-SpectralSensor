66fbfcf36ec054dfc5531ed7df60428aa76cf5be
kaynelua <luayinghao95@hotmail.com>
2019-02-01 16:12:38 +0000

    Working implementation of sensor classes

--- ProximitySensor.py
+++ ProximitySensor.py
@@ -13,12 +13,12 @@ class ProximitySensor:
 		self.ledLevel(20)
 
 	def readProximity(self):
-		prox.write(bus,0x80,0x08)
-        while(not (prox.read(bus,0x80)&0x20)):
-                pass
-        h = prox.read(bus,0x87)
-        d = h << 8 | prox.read(bus,0x88)
-
-    def ledLevel(self,lvl:int):
-    	if(lvl >=0 and lvl <= 20):
-    		prox.write(bus,0x83,lvl)
+		write(self.bus,0x80,0x08)
+		while(not (read(self.bus,0x80)&0x20)):
+			pass
+		h = read(self.bus,0x87)
+		d = h << 8 | read(self.bus,0x88)
+		return d
+	def ledLevel(self,lvl:int):
+		if(lvl >=0 and lvl <= 20):
+			write(self.bus,0x83,lvl)
