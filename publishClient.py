85cefcc1dab8e6496cf8ac360c7ef8cdf167cb14
kaynelua <luayinghao95@hotmail.com>
2019-02-01 13:51:19 +0000

    Set up client MQTT

--- publishClient.py
+++ publishClient.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 client = mqtt.Client()
-client.tls_set(ca_certs="mosquitto.org.crt",
-	certfile="client.crt",keyfile="client.key")
+client.tls_set(ca_certs="keys/mosquitto.org.crt",
+	certfile="keys/client.crt",keyfile="keys/client.key")
 client.connect("test.mosquitto.org",port=8883)
 
 MSG_INFO = client.publish("IC.Embedded/IOS/test","hello")
