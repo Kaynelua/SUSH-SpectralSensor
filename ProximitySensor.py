d243c5d1382ea8c8ae75f6bb3526994759890fac
patrickjohncyh <patrickjohncyh@icloud.com>
2019-02-07 14:54:38 +0000

    Add intterupt functionality to proximity sensor class and add main file

--- ProximitySensor.py
+++ ProximitySensor.py
@@ -19,6 +19,25 @@ class ProximitySensor:
 		h = read(self.bus,0x87)
 		d = h << 8 | read(self.bus,0x88)
 		return d
+
 	def ledLevel(self,lvl:int):
 		if(lvl >=0 and lvl <= 20):
 			write(self.bus,0x83,lvl)
+
+	def proxInterrupt(self,state=0):
+		if(state):
+			write(self.bus,0x89,2)	# Enable interrupt 
+		else:
+			write(self.bus,0x89,0)  # Disable interrupt 
+
+	def setLowThreshold(self,t):
+		highBits = (0xFF00 & t) >> 6
+		lowBits  = (0x00FF & t)
+		write(self.bus,0x8A,highBits)	# Enable interrupt 
+		write(self.bus,0x8B,lowBits)	# Enable interrupt 
+
+	def setHighThreshold(self,t):	
+		highBits = (0xFF00 & t) >> 6
+		lowBits  = (0x00FF & t)
+		write(self.bus,0x8C,2)	# Enable interrupt 
+		write(self.bus,0x8D,2)	# Enable interrupt 	
---
+++ main.py
@@ -0,0 +1,57 @@
+import time
+import RPi.GPIO as GPIO
+import SpectralSensor as ss
+import ProximitySensor as ps
+from threading import Thread
+import Servo as servo
+
+s = ss.SpectralSensor()
+p = ps.ProximitySensor()
+
+def sensorEvent(pin):
+	s.ledDrv(1)
+	s.setBank(3)
+	r = s.readAllCal()
+	s.ledDrv(0)
+	print(r)
+	servo.open()
+	servo.close()
+
+
+interruptPin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(interruptPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(17,GPIO.FALLING,callback=sensorEvent)
+
+
+
+count = 0
+while(True):
+	try:
+		count = count + 1
+		print("Doing Other Stuff " + str(count))
+		time.sleep(0.1);
+	except:
+		GPIO.cleanup()
+
+
+
+
+'''
+s = ss.SpectralSensor()
+p = ps.ProximitySensor()
+
+
+while(1):
+        reading = p.readProximity()
+         if(reading > 10000):
+                s.ledDrv(1)
+                s.setBank(3)
+                r = s.readAllCal()
+                s.ledDrv(0)
+                print(r)
+                dictData = {'sensorReadings':r}
+                payload = json.dumps(dictData)
+                MSG_INFO = client.publish("IC.Embedded/IOS/"+tableID,payload)
+        time.sleep(0.1)
+'''
