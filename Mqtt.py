0ed9ef8cb395c774970dd158d5c2160f5055f71c
patrickjohncyh <patrickjohncyh@icloud.com>
2019-02-08 17:45:07 +0000

    Fix syntax errors

--- Mqtt.py
+++ Mqtt.py
@@ -9,14 +9,14 @@ class Mqtt():
 		self.client = mqtt.Client(transport = "websockets")
 		self.client.tls_set(certfile="client.crt",keyfile="client.key")
 		self.client.on_connect = self.on_connect
-		self.client..connect("test.mosquitto.org",port=8081)
+		self.client.connect("test.mosquitto.org",port=8081)
 
 	def on_connect(self,client, userdata, flags, rc):
    		print("Succesful with result code "+str(rc))
     	# Subscribing in on_connect() means that if we lose the connection and
     	# reconnect then subscriptions will be renewed.
-    	client.subscribe("IC.Embedded/IOS/#")
+    	self.client.subscribe("IC.Embedded/IOS/#")
     	print(mqtt.error_string(MSG_INFO.rc))
 
 	def send(self,topic,payload):
-		MSG_INFO = client.publish(topic,payload)   	
+		MSG_INFO = self.client.publish(topic,payload)   	
