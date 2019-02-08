a9161623a807382cb11054099f9e13cbf31d6df8
Kaynelua <luayinghao95@hotmail.com>
2019-02-08 16:27:08 +0000

    New publish client using port 8883 encrypted websocket

--- publishClient.py
+++ publishClient.py
@@ -1,9 +1,8 @@
 import paho.mqtt.client as mqtt
-client = mqtt.Client()
-client.tls_set(ca_certs="keys/mosquitto.org.crt",
-	certfile="keys/client.crt",keyfile="keys/client.key")
-client.connect("test.mosquitto.org",port=8883)
+client = mqtt.Client(transport = "websockets")
+client.tls_set(certfile="client.crt",keyfile="client.key")
+client.connect("test.mosquitto.org",port=8081)
 
-MSG_INFO = client.publish("IC.Embedded/IOS/test","hello")
+MSG_INFO = client.publish("IC.Embedded/IOS/test","123hello")
 
 print(mqtt.error_string(MSG_INFO.rc))
