b6be0ae602f77ce59e8d42692cfb617d521b531c
patrickjohncyh <patrickjohncyh@icloud.com>
2019-02-08 11:02:08 +0000

    Trial plate recognitiion training

--- PlateDetector.py
+++ PlateDetector.py
@@ -7,6 +7,7 @@ import queue
 import Servo as servo
 
 class PlateDetector():
+
 	def __init__(self,mode='Train'):
 		GPIO.setmode(GPIO.BCM)
 		self.s = ss.SpectralSensor()
@@ -14,36 +15,62 @@ class PlateDetector():
 		self.interruptPin = 17
 		self.mode = 'Train'
 		self.resultQueue = queue.Queue()
+		self.p.setHighThreshold(10000)
 		GPIO.setup(self.interruptPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 		GPIO.add_event_detect(self.interruptPin,GPIO.FALLING,callback=self.sensorEvent)
-		self.p.setHighThreshold(10000)
 		self.p.setInterrupt(1)
 
+		self.refValues = {}
+
+	def autoScanning(self):
+		pass
+
+	def train(self,c):
+		val = pd.getResult()
+		if c not in self.refValues.keys():
+			self.refValues[c] = (val,1)
+		else:
+			(q,n) = self.refValues[c]
+			newVal = q + (1/(n+1))*(val-q)
+			self.refValues[c] = (newVal,n+1)
+
+
+
+
 	def sensorEvent(self,pin):
+		# Read Sensor and Store in Queue
 		self.s.ledDrv(1)
 		self.s.setBank(3)
 		r = self.s.readAllCal()
+		self.resultQueue.put_nowait(r)
 		self.s.ledDrv(0)
+		
+		# Open and Close to release plate
 		servo.open()
 		time.sleep(0.1)
 		servo.close()
+
+		# Reset Interrupt on Proximity Sensor
 		GPIO.remove_event_detect(self.interruptPin)
 		self.p.setInterrupt(0)
 		GPIO.add_event_detect(self.interruptPin,GPIO.FALLING,callback=self.sensorEvent)
-#		self.p.setInterrupt(0)
 		self.p.setInterrupt(1)
-		self.resultQueue.put_nowait(r)
+
 	def getResult(self):
 		return self.resultQueue.get(block=True)
+
+	def 
+
+
 
 count = 0
 pd = PlateDetector()
 while(True):
 	try:
-		
-		print(pd.getResult())
-		print("yinghao senpai")
-		time.sleep(8)
+		#print(pd.getResult())
+		#time.sleep(8)
+		pd.train("Orange")
+		print pd.refValues()
 	except:
 		GPIO.cleanup()
 
