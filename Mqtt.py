f2042287c274d4f5e4c3324f17828b076ca667b6
kaynelua <luayinghao95@hotmail.com>
2019-02-13 18:27:24 +0000

    Final push

--- Mqtt.py
+++ Mqtt.py
@@ -1,18 +1,18 @@
 import paho.mqtt.client as mqtt
 
-MSG_INFO = client.publish("IC.Embedded/IOS/test","123hello")
+#MSG_INFO = client.publish("IC.Embedded/IOS/test","123hello")
 
 
 class Mqtt():
 
 	def __init__(self):
 		self.client = mqtt.Client(transport = "websockets")
-		self.client.tls_set(certfile="client.crt",keyfile="client.key")
+		self.client.tls_set(certfile="keys/client.crt",keyfile="keys/client.key")
 		self.client.on_connect = self.on_connect
 		self.client.connect("test.mosquitto.org",port=8081)
 
 	def on_connect(self,client, userdata, flags, rc):
-   		print("Succesful with result code "+str(rc))
+		print("Succesful with result code "+str(rc))
     	# Subscribing in on_connect() means that if we lose the connection and
     	# reconnect then subscriptions will be renewed.
 		self.client.subscribe("IC.Embedded/IOS/#")
